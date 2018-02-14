cac369593e1cb61b8c4dfc36ed7d59c391a0c4d0
Becky McClain <beckykelley8@yahoo.com>
2018-02-14 10:59:03 -0700

    almost done

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+//array of letters for the computer to choose from
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesSoFar = [""]
+
+// computer chooses a random letter
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log(computerGuess);
+
+// player guesses letters
+document.onkeyup = function (event) {
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess);
+    // make an if statement that says they should only click letters... no other keys
+
+    // app records what letters have been guessed so far
+    guessesSoFar.push(userGuess)
+
+
+    // if/then statement for wins and losses
+    if (userGuess === computerGuess) {
+
+        wins++;
+        console.log("win!");
+        // make sure game restarts after each win/loss w/o refreshing the game
+        function resetGame() {
+            guessesLeft = 10;
+            computerGuess = letters[Math.floor(Math.random() * letters.length)];
+            console.log(computerGuess);
+            guessesSoFar = [""]
+        }
+
+        resetGame();
+    }
+
+    else if (guessesLeft === 0) {
+        losses++;
+        function resetGame() {
+            guessesLeft = 10;
+            computerGuess = letters[Math.floor(Math.random() * letters.length)];
+            console.log(computerGuess);
+            guessesSoFar = [""]
+        }
+
+        resetGame();
+    }
+  
+
+    var html =
+        "<h2>Guess What Letter I'm Thinking Of!</h2>" +
+
+        "<p>Wins: " + wins + "</p>" +
+
+        "<p>Losses: " + losses + "</p>" +
+
+        "<p>Guesses Left: " + guessesLeft-- + "</p>" +
+
+        "<p>Your Guesses So Far: " + guessesSoFar + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+
+}
