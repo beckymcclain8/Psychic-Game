77ded1e079b4e9655f5af0419235f733147ccaf1
Becky McClain <beckykelley8@yahoo.com>
2018-02-17 16:15:26 -0700

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
+var guessesLeft = 9;
 var guessesSoFar = [""]
 
 // computer chooses a random letter
@@ -13,52 +13,65 @@ console.log(computerGuess);
 document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
     console.log(userGuess);
-    // make an if statement that says they should only click letters... no other keys
+
 
     // app records what letters have been guessed so far
     guessesSoFar.push(userGuess)
 
 
-    // if/then statement for wins and losses
-    if (userGuess === computerGuess) {
+// Trying to make an if statement that says they should only click letters... no other keys
+    if (letters.includes(userGuess)) {
+        // if/then statement for wins and losses
+        if (userGuess === computerGuess) {
 
-        wins++;
-        console.log("win!");
-        // make sure game restarts after each win/loss w/o refreshing the game
-        function resetGame() {
-            guessesLeft = 10;
-            computerGuess = letters[Math.floor(Math.random() * letters.length)];
-            console.log(computerGuess);
-            guessesSoFar = [""]
+            wins++;
+            console.log("win!");
+            // make sure game restarts after each win/loss w/o refreshing the game
+            function resetGame() {
+                guessesLeft = 10;
+                computerGuess = letters[Math.floor(Math.random() * letters.length)];
+                console.log(computerGuess);
+                guessesSoFar = [""]
+            }
+
+            resetGame();
         }
 
-        resetGame();
-    }
+        else if (guessesLeft === 0) {
+            losses++;
+            function resetGame() {
+                guessesLeft = 10;
+                computerGuess = letters[Math.floor(Math.random() * letters.length)];
+                console.log(computerGuess);
+                guessesSoFar = [""]
+            }
 
-    else if (guessesLeft === 0) {
-        losses++;
-        function resetGame() {
-            guessesLeft = 10;
-            computerGuess = letters[Math.floor(Math.random() * letters.length)];
-            console.log(computerGuess);
-            guessesSoFar = [""]
+            resetGame();
         }
 
-        resetGame();
+
+        var html =
+            "<h2>Guess What Letter I'm Thinking Of!</h2>" +
+            
+            '<img src="assets/images/question.gif" alt="Kids questioning" id="question">' +
+
+            "<p>Wins: " + wins + "</p>" +
+
+            "<p>Losses: " + losses + "</p>" +
+
+            "<p>Guesses Left: " + guessesLeft-- + "</p>" +
+
+            "<p>Your Guesses So Far: " + guessesSoFar + "</p>";
+
+        document.querySelector("#game").innerHTML = html;
+
     }
-  
 
-    var html =
-        "<h2>Guess What Letter I'm Thinking Of!</h2>" +
-
-        "<p>Wins: " + wins + "</p>" +
-
-        "<p>Losses: " + losses + "</p>" +
-
-        "<p>Guesses Left: " + guessesLeft-- + "</p>" +
-
-        "<p>Your Guesses So Far: " + guessesSoFar + "</p>";
-
-    document.querySelector("#game").innerHTML = html;
-
+    else if (userGuess !== letters) {
+        alert("You must choose a letter!")
+    }
 }
+
+
+
+
